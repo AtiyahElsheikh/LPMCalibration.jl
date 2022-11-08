850763116e5d39b20b0a5e44969db749e58af9bb
Atiyah <a.m.g.elsheikh@gmail.com>
2022-11-08 14:25:34 +0000

    active parametes and model definitions

--- main.jl
+++ main.jl
@@ -1,41 +1,196 @@
 """
 Initial draft of calibrating an LPM-based model
+It is assumed that LoneParentsModel.jl is located in the following relative path ../LoneParentsModel.jl
+This version works with LoneParentsModel.jl version TAG-Verion 0.6.0
+                                                    =================
+later version of LoneParentsModel may be not compatible (simply switch to TAG-Version) 
+can be directly executing as 
+# julia main.jl 
+or within REPL as 
+julia> include("main.jl")
 """ 
 
-include("./utilities.jl")
 
-# Step I 
-# adding paths to the LPM and other nonstandard packages 
-# here it will be assumed that LPM.jl is located in the following relative path ../LPM.jl 
-# ... 
+# Step I - OK 
+# ===========  
+#       adding paths to the LPM and other nonstandard packages 
+#
 
-# Step II 
-# determine the active parameters of the LPM 
-# i.e. which parameters are of interest and shall be variated from a simulation to another 
-# a. initially this could be done manually as a proof of concept 
-# b. later: active parameters to be set in from an input file with flags  
+# Viewing LPM as a library    
+module LPMLib
+    # export VERSION
+    export LPMPATH 
+    export loadAndSeedSimulationPars, loadModelParameters
+
+    # const VERSION = r"V0.6.0"   
+    
+    include("./utilities.jl")
+    const LPMPATH = "../LoneParentsModel.jl"
+    addToLoadPath!(LPMPATH) 
+    addToLoadPath!("$(LPMPATH)/src")
+    addToLoadPath!("$(LPMPATH)/src/generic")
+
+    using LPM.ParamTypes: SimulationPars, seed!, DemographyPars
+
+    # TODO some significant (simulation & model) parameters 
+    #   could be made as flags but not necessarily all (e.g. matrices)
+    #   to make it possible to load from an input file 
+    function loadAndSeedSimulationPars() 
+        simPars = SimulationPars() # default values  
+        simPars.seed = 0           # Every execution will be different 
+        seed!(simPars)             
+        simPars 
+    end 
+
+    function loadModelParameters()
+        pars = DemographyPars() 
+        # adhoc fix: todo improve
+        pars.datapars.datadir =  "$(LPMPATH)/$(pars.datapars.datadir)"  
+        pars 
+    end
+
+# Step II-  OK 
+# ============
+#       establish model definition to be used for calibration 
+#       as well as simulation parameters 
+#       every time the model is calibrated a deep copy instance is employed  
+
+    module ModelDef
+        using ..LPMLib: LPMPATH, loadModelParameters, addToLoadPath!, loadAndSeedSimulationPars
+        include("$(LPMPATH)/mainHelpers.jl")
+
+        export MODEL, MODPARS, SIMPARS, Model 
+
+        # simulation parameters defining the 
+        const SIMPARS =  loadAndSeedSimulationPars() 
+
+        # model parameter definitions that will be variated from a simulation to another 
+        const MODPARS  = loadModelParameters()
+
+        # This is a model definition that will be deep copied for every simulation instance 
+        const MODEL = setupModel(MODPARS)
+    end # ModelDefinition
+    
+end # LPMLib
+
+# if the above package is moved to LoneParentsModel.jl 
+# @assert LoneParentsModel.VERSION == r"V0.6.0"  
+
 
 # Step III 
-# Set an upper and lower bounds of every active parameter p, p_min and p_max   
-# a. initially manually hard-coded 
-# b. later: from an input file and flags etc. 
+# ========
+#       determine the active parameters of the LPM , their lower bounds and upper bouns 
+#       i.e. which parameters are of interest and shall be variated from a simulation to another 
+#       a. initially this could be done manually as a proof of concept (OK)
+#       b. later: active parameters to be set in from an input file with flags  
 
-# Step IV 
-# set up time-stamped simulation folder for placing (if needed) the results 
+mutable struct ActiveParameter{ValType} 
+    lowerbound::ValType 
+    upperbound::ValType  
+    group::Symbol
+    name::Symbol         
+    
+    function ActiveParameter{ValType}(low,upp,gr,id) where ValType 
+        @assert low <= upp 
+        new(low,upp,gr,id)
+    end
+end 
+
+# TODO Base.show methods 
+
+# as a hint @Umberto
+# The following is how you can find out the group names and parameter names 
+# from REPL 
+# julia> include("main.jl")
+# julia> using .LPMLib: DemographyPars
+# julia> using .LPMLib.ModelDef: MODPARS 
+# julia> println(fieldnames(DemographyPars))
+# julia> println(fieldnames(typeof(MODPARS.poppars))) 
+#  
+
+# adhoc choosing 7 active parameters 
+# TODO: choices and values to be loaded from a file 
+const femaleAgeScaling      = ActiveParameter{Float64}(10.0,20,:poppars,:femaleAgeScaling) 
+const maleAgeScaling        = ActiveParameter{Float64}(5.0,15.0,:poppars,:maleAgeScaling)
+
+const femaleMortalityBias   = ActiveParameter{Float64}(0.77,0.89,:poppars,:femaleMortalityBias)
+const maleMortalityBias     = ActiveParameter{Float64}(0.71,0.83,:poppars,:maleMortalityBias)
+
+const variableDivorce       = ActiveParameter{Float64}(0.01,0.14,:divorcepars,:variableDivorce)
+const basicDivorceRate      = ActiveParameter{Float64}(0.01,0.15,:divorcepars,:basicDivorceRate)
+
+const basicMaleMarraigeProb = ActiveParameter{Float64}(0.5,0.9,:marriagepars,:basicMaleMarriageProb)
+
+# TODO: to add meaningful constraints, e.g. 
+#       femaleAgeScaling > maleAgeScaling + something
+#       maleMortalityBias < femaleMortalityBias - something  
+ 
+const activePars = [femaleAgeScaling, maleAgeScaling, femaleMortalityBias, maleMortalityBias,
+                    variableDivorce, basicDivorceRate, 
+                    basicMaleMarraigeProb]
+
+
+# Step III 
+# ========
+# generate a set of models and parameters for multiple simulations 
+
+# TODO configuring using flags / input files 
+const NUMSIMS = 100    # number of simulations 
+
+function setParValueExp(pname::String, active::ActiveParameter{ValType}, val::ValType) where ValType  
+    @assert val >= active.lowerbound && val <= active.upperbound
+    gr = active.group 
+    id = active.name  
+    Meta.parse("$(pname).$(gr).$(id) = $(val)")
+end 
+
+using Distributions: Uniform 
+function setRandParValueExp(pname,active) 
+    val = rand(Uniform(active.lowerbound,active.upperbound))
+    setParValueExp(pname,active,val)
+end
+
+setRandParValue!(pname,active) = eval(setRandParValueExp(pname,active))
+
+# establish model parameter sets according to active parameters 
+
+using .LPMLib.ModelDef: Model, MODPARS 
+
+# TODO establish a function that generates random parameters  
+#   need to take care that this makes eval(exp) become local to a function 
+
+using .LPMLib: DemographyPars, SimulationPars
+const parameters =  DemographyPars[] 
+const models     =  Model[] 
+# const simpars    =  SimulationPars[]   # probably not needed, it is the same across all simulations 
+
+for index in 1:NUMSIMS 
+    push!(parameters,deepcopy(MODPARS)) 
+    for active in activePars 
+        setRandParValue!("parameters[$index]", active)
+    end
+    push!(models,deepcopy(MODEL)) 
+    #println(parameters[index].poppars.femaleAgeScaling)
+end
+
+ 
+# Step IV
+# =======
+# Establish / load data to which the model definition is going to be calibrated 
 #   & placing the imperical data as well as other related stuffs 
 #   & which model variables do they correspond to 
 # a. initially hard-coded 
 # b. later from input files / flags 
 
-# Step V 
-# solution-dependent 
+# Step V
 # initially define a cost function (e.g. sum of least squares) 
 # can be also a vector rather than a single value depending on the imperical data 
 
 # Step VI 
 # conduct calibration multiple simulation
 # a. initially brute-force naive technique 
-# b. Other suggested packatges, e.g. hypercube 
+# b. Other suggested packatges, e.g. hypercube, differential evolution .. 
+# ... 
 
 
 
